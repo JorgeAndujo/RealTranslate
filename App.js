9ae5c0a2cd05281cb9d0c22b8c47f57da6948d6b
Jorge Andujo <jandujo@interaxis.com.mx>
2023-10-25 23:00:33 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SavedScreen from "./screens/SavedScreen";
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+        }}
+      />
+      <Tab.Screen
+        name="saved"
+        component={SavedScreen}
+        options={{
+          tabBarLabel: "Guardado",
+        }}
+      />
+      <Tab.Screen
+        name="settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: "Opciones",
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1 }}>
+        <Stack.Navigator>
+          <Stack.Group>
+            <Stack.Screen
+              name="home"
+              component={TabNavigator}
+              options={{
+                headerTitle: "RealTranslate",
+              }}
+            />
+          </Stack.Group>
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen(props) {
+  return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+
+        <Button
+          title="Click me" onPress={() => {
+            props.navigation.navigate("settings")
+          }}
+        />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
